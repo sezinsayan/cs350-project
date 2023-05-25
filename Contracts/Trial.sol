2b7f4a84dbd41a82c0503f14bc5393e4f1c9e097
sezinsayan <sezin.sayan@ozu.edu.tr>
2023-05-25 22:03:59 +0300

    update

--- Contracts/Trial.sol
+++ Contracts/Trial.sol
@@ -1,11 +1,26 @@
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.18;
 
 contract Trial{
     address public admin;
     address public recipient;
 
-    constructor(address _recipient) {
+    constructor() {
         admin = msg.sender;
+    }
+
+    function setRecipient(address _recipient) public {
+        require(msg.sender == admin, "Only the admin can set the recipient");
         recipient = _recipient;
     }
+
+    function transfer(uint256 amount) public {
+        require(msg.sender == admin, "Only the admin can make transfers");
+        require(address(this).balance >= amount, "Not enough balance in account");
+
+        (bool success, ) = recipient.call{value: amount}("");
+        require(success, "Transfer failed");
+    }
+
+    receive() external payable {}
+}
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,10 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  networks: {
+    ganache: {
+      url: "http://localhost:8545", // Replace with the correct Ganache URL
+      chainId: 1337, // Replace with the correct chain ID of Ganache network
+    },
+  },
   solidity: "0.8.18",
 };
