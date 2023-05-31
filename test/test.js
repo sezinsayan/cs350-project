67c37de42be527138e2adaa0723d015301b8baa2
sezinsayan <sezin.sayan@ozu.edu.tr>
2023-05-31 20:42:07 +0300

    g

--- test/test.js
+++ test/test.js
@@ -14,6 +14,13 @@ describe("Trial Contract", function () {
     const Contract = await ethers.getContractFactory("Trial");
     contract = await Contract.deploy();
     await contract.deployed();
+
+    // Fund the contract with Ether
+    const fundingAmount = ethers.utils.parseEther("10.0");
+    await admin.sendTransaction({
+      to: contract.address,
+      value: fundingAmount,
+    });
   });
 
   describe("Correct address", function () {
@@ -27,12 +34,13 @@ describe("Trial Contract", function () {
     it("should transfer funds to the recipient", async function () {
       const recipientAddress = "0x14dC79964da2C08b23698B3D3cc7Ca32193d9955";
       const amount = ethers.utils.parseEther("1.0");
-
+    
       const initialBalance = await recipient.getBalance();
       await contract.connect(admin).transfer(amount);
-
+    
       const newBalance = await recipient.getBalance();
-      expect(newBalance.sub(initialBalance)).to.equal(amount);
+      expect(newBalance.sub(initialBalance).toString()).to.equal(amount.toString());
     });
+    
   });
 });
