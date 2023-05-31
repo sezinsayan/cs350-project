e0c465b6b69625af956acea9e453747f9cfc88d5
sezinsayan <sezin.sayan@ozu.edu.tr>
2023-05-31 20:38:00 +0300

    1 test passed

--- test/test.js
+++ test/test.js
@@ -10,29 +10,26 @@ describe("Trial Contract", function () {
     admin = deployer;
     recipient = recipientAddr;
 
-    // Get the contract address from the deployment script
-    const contractAddress = admin.address; // Replace with the actual contract address
-
+    // Deploy the contract
     const Contract = await ethers.getContractFactory("Trial");
-    contract = await Contract.attach(contractAddress);
+    contract = await Contract.deploy();
+    await contract.deployed();
   });
 
   describe("Correct address", function () {
     it("should set the recipient address", async function () {
-      // Access the recipient address from the deployment script
       const recipientAddress = "0x14dC79964da2C08b23698B3D3cc7Ca32193d9955";
 
-      await contract.setRecipient(recipientAddress);
+      await contract.connect(admin).setRecipient(recipientAddress);
       expect(await contract.recipient()).to.equal(recipientAddress);
     });
 
     it("should transfer funds to the recipient", async function () {
-      // Access the recipient address and amount from the deployment script
       const recipientAddress = "0x14dC79964da2C08b23698B3D3cc7Ca32193d9955";
       const amount = ethers.utils.parseEther("1.0");
 
       const initialBalance = await recipient.getBalance();
-      await contract.transfer(amount);
+      await contract.connect(admin).transfer(amount);
 
       const newBalance = await recipient.getBalance();
       expect(newBalance.sub(initialBalance)).to.equal(amount);
