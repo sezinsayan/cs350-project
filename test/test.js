06efb691fded20a9a7bab2b4ec138f16b61cdf29
sezinsayan <sezin.sayan@ozu.edu.tr>
2023-05-31 19:16:36 +0300

    test fix

--- test/test.js
+++ test/test.js
@@ -25,25 +25,28 @@ describe("Trial Contract", function () {
       to: recipient.address,
       value: ethers.utils.parseEther("10"), // Amount of Ether to send
     });
-  });
+  
+  describe("Correct address", function () {
+    it("should set the recipient address", async function () {
+      // Access the recipient address from the deployment script
+      const recipientAddress = "0x14dC79964da2C08b23698B3D3cc7Ca32193d9955";
 
-  it("should set the recipient address", async function () {
-    // Access the recipient address from the deployment script
-    const recipientAddress = "0x14dC79964da2C08b23698B3D3cc7Ca32193d9955";
+      await contract.setRecipient(recipientAddress);
+      expect(await contract.recipient()).to.equal(recipientAddress);
+    });
+  })
+  describe("Checking transfer", function(){
+    it("should transfer funds to the recipient", async function () {
+      // Access the recipient address and amount from the deployment script
+      const recipientAddress = "0x14dC79964da2C08b23698B3D3cc7Ca32193d9955";
+      const amount = ethers.utils.parseEther("1.0");
 
-    await contract.setRecipient(recipientAddress);
-    expect(await contract.recipient()).to.equal(recipientAddress);
-  });
+      const initialBalance = await recipient.getBalance();
+      await contract.transfer(amount);
 
-  it("should transfer funds to the recipient", async function () {
-    // Access the recipient address and amount from the deployment script
-    const recipientAddress = "0x14dC79964da2C08b23698B3D3cc7Ca32193d9955";
-    const amount = ethers.utils.parseEther("1.0");
-
-    const initialBalance = await recipient.getBalance();
-    await contract.transfer(amount);
-
-    const newBalance = await recipient.getBalance();
-    expect(newBalance.sub(initialBalance)).to.equal(amount);
-  });
+      const newBalance = await recipient.getBalance();
+      expect(newBalance.sub(initialBalance)).to.equal(amount);
+    });
+  })
+});
 });
